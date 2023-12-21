9935109a69109638bd2bfc84a027a28711dccc28
medyll <43032494+medyll@users.noreply.github.com>
2023-12-21 01:17:14 +0100

    chore: api route for ssr

---
+++ src/routes/api/ollama/+server.ts
@@ -0,0 +1,10 @@
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async () => {
+    return new Response();
+};
+
+export const POST: RequestHandler = async (event) => {
+    
+    return new Response();
+};
