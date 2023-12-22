739202ca0044ece63ecd9b8e61d816459947e582
medyll <43032494+medyll@users.noreply.github.com>
2023-12-22 13:13:00 +0100

    chore: Refactor configuration.js file

--- src/configuration/configuration.js
+++ src/configuration/configuration.js
@@ -1,39 +1,37 @@
 
-
 export const defaultOllamaSettings = {
-    seed: undefined,
-    temperature: 0.5,
-    repeat_penalty: undefined,
-    top_k: undefined,
-    top_p: undefined,
-    num_ctx: undefined,
-}
+	seed: undefined,
+	temperature: 0.5,
+	repeat_penalty: undefined,
+	top_k: undefined,
+	top_p: undefined,
+	num_ctx: undefined
+};
 
-export const defaultOptions  = {
+export const defaultOptions = {
 	defaultModel: 'llama2-uncensored',
-    ollama_server: 'http://127.0.0.1:11434',
-    authHeader: false,
-    system_prompt: '',
-    request_mode: 'plain',
-    title_auto: true,
-    voice_auto_stop: true,
-    avatar_email: '',
+	ollama_server: 'http://127.0.0.1:11434',
+	authHeader: false,
+	system_prompt: '',
+	request_mode: 'plain',
+	title_auto: true,
+	voice_auto_stop: true,
+	avatar_email: '',
 	sender: {
 		speechAutoSend: false,
 		speechRecognition: true
-	}, 
-    temperatures:{
-        creative: 0.1,
-        balanced: 0.5,
-        accurate: 1,
-    },
-    llamaOptions:{
-        seed: undefined,
-        temperature: 0.5,
-        repeat_penalty: undefined,
-        top_k: undefined,
-        top_p: undefined,
-        num_ctx: undefined,   
-    }
+	},
+	temperatures: {
+		creative: 0.1,
+		balanced: 0.5,
+		accurate: 1
+	},
+	llamaOptions: {
+		seed: undefined,
+		temperature: 0.5,
+		repeat_penalty: undefined,
+		top_k: undefined,
+		top_p: undefined,
+		num_ctx: undefined
+	}
 };
-
