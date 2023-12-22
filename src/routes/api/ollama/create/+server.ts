f0978214f15aa7d51e26d18952d383c1a3362c2c
medyll <43032494+medyll@users.noreply.github.com>
2023-12-22 13:06:16 +0100

    feat: Add POST request handler and insert chat data

--- src/routes/api/ollama/create/+server.ts
+++ src/routes/api/ollama/create/+server.ts
@@ -1,5 +1,9 @@
+import { dbQuery } from '$lib/db/dbQuery';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async () => {
-    return new Response();
+export const POST: RequestHandler = async ({ request }) => {
+    await request.json();
+	dbQuery.insertChat();
+    
+	return new Response();
 };
