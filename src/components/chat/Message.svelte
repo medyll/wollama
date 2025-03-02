d7fe9505d55f7eeedb2d5ac3c6c50ee348dfdb6c
medyll <43032494+medyll@users.noreply.github.com>
2025-03-02 20:30:11 +0100

    fix: gérer les valeurs nulles pour les propriétés de message dans Message.svelte

--- src/components/chat/Message.svelte
+++ src/components/chat/Message.svelte
@@ -19,8 +19,8 @@
     let { messageId }: MessageProps = $props();
     let message = $derived<DBMessage>(messageId ? idbQuery.getMessage(messageId) : ({} as DBMessage));
 
-    let icon = $derived(message.role === 'user' ? 'lets-icons:user-scan-light' : 'icon-park:robot-one');
-    let place = $derived(message.role === 'user' ? 'mr-24' : 'ml-24');
+    let icon = $derived(message?.role === 'user' ? 'lets-icons:user-scan-light' : 'icon-park:robot-one');
+    let place = $derived(message?.role === 'user' ? 'mr-24' : 'ml-24');
 
     marked.use({
         async: false,
@@ -75,16 +75,17 @@
     }
 
     let assistantCode = $derived.by(() => {
-        if (message?.role == 'assistant' && message?.content && message.content.length) {
+        if (message?.role === 'assistant' && message?.content && message?.content?.length) {
             return selectCodeTags(message?.content);
         }
     });
 
-    let dd = $derived(message?.created_at.getTime().toString());
-    let order = $derived(`order: ${dd};`);
+    let dd = $derived(message?.created_at?.getTime().toString()); 
+ 
+
 </script>
-<!-- {place} -->
-<div  class="message   message-{message.role}">
+<!-- {place} --> 
+<div  class="message   message-{message?.role}">
     <!-- <div class="p-1">
         <div class="p-2 rounded-full shadow-md theme-border bg-gray-50/10">
             <Icon style="font-size:1.6em" {icon} />
@@ -96,34 +97,34 @@
             </div>
         </div>
     {/if} -->
-    <div class="flex flex-col w-full"> {message?.created_at}
+    <div class="flex flex-col w-full"> 
         <div class="line-gap-2 mb-1 p-1 {message?.role == 'assistant' ? 'flex-row-reverse' : ''}">
             
             <div class="soft-title">
                 {#if message?.status == 'streaming'}<Icon style="font-size:1.6em" icon="mdi:reload" class="spin" />{/if}
             </div>
-           <!--  <div class="soft-title">{message?.model ?? ''}</div> -->
+           <!--  <div class="soft-title">{message?.model ?? ''}</div> format(new Date(message?.created_at), 'dd MMMM y hh:mm')-->
             <div class="flex-1"></div>
             <div class="soft-title">{message?.status != 'done' ? message?.status : ''}</div>
-            <div class="soft-title">{format(new Date(message?.created_at), 'dd MMMM y hh:mm')}</div>
+            <div class="soft-title">{message?.created_at}</div>
             <div class="p-2 rounded-full shadow-md theme-border bg-gray-50/10">
             <Icon style="font-size:1.6em" {icon} />
         </div>
         </div>
         <div style="user-select:text;" class="speech-bubble theme-border preserve-line-breaks">
-            {#if message.urls?.length}
-                <Looper class="flex-h" data={message.urls}>
+            {#if message?.urls?.length}
+                <Looper class="flex-h" data={message?.urls}>
                     {#snippet children({ item })}
                         <div>{item.title}</div>
                     {/snippet}
                 </Looper>
                 <hr />
             {/if}
-            {#if message.images}
-                <img src={message.images.dataUri} alt="list" style="height:100px" />
+            {#if message?.images?.length}
+                <img src={message?.images?.dataUri} alt="list" style="height:100px" />
             {/if}
-            {#if message?.role == 'assistant'}
-                {#if message.status == 'idle'}
+            {#if message?.role === 'assistant'}
+                {#if message?.status === 'idle'}
                     <Skeleton class="h-full" />
                 {:else if ['streaming', 'done'].includes(message.status)}
                     <!-- {@html assistantCode} -->
