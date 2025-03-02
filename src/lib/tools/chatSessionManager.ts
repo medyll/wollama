6e41d5fb5cd85a2964555a2d9268fea54518da03
medyll <43032494+medyll@users.noreply.github.com>
2025-03-02 20:28:59 +0100

    feat: ajouter un nouveau rôle 'tool' dans l'énumération OllamaChatMessageRole et créer un gestionnaire de session de chat

--- src/lib/tools/chatSessionManager.ts
+++ src/lib/tools/chatSessionManager.ts
@@ -1,217 +1,273 @@
-import { idbQuery } from "$lib/db/dbQuery";
-import type { DbChat, DBMessage, MessageImageType } from "$types/db";
-import { OllamaChatMessageRole, type OllamaChatMessage } from "$types/ollama";
+import { idbQuery } from '$lib/db/dbQuery';
+import type { DbChat, DBMessage, MessageImageType } from '$types/db';
+import { OllamaChatMessageRole, type OllamaChatMessage } from '$types/ollama';
+import type { Message, ToolCall } from 'ollama/browser';
 
 export class ChatSessionManager {
-  #sessionId?: number;
-  #SessionDB: {
-    dbChat: Partial<DbChat>;
-    userChatMessage?: OllamaChatMessage;
-    userDbMessage?: DBMessage;
-    messageAssistant: DBMessage[];
-    messageUser: Partial<DBMessage>;
-  } = {
-    dbChat: {},
-    messageAssistant: [],
-    messageUser: {},
-    userChatMessage: {} as OllamaChatMessage,
-    userDbMessage: {} as DBMessage,
-  };
-  #sessionConfig: { apiType: "generate" | "chat"; useContext: boolean } = {
-    apiType: "generate",
-    useContext: false,
-  };
+	#SessionDB: {
+		sessionId?: number;
+		dbChat: Partial<DbChat>;
+		userChatMessage?: OllamaChatMessage;
+		userDbMessage?: DBMessage;
+		messageAssistant: DBMessage[];
+		messageUser: Partial<DBMessage>;
+	} = {
+		dbChat: {},
+		messageAssistant: [],
+		messageUser: {},
+		userChatMessage: {} as OllamaChatMessage,
+		userDbMessage: {} as DBMessage
+	};
+	#sessionConfig: { apiType: 'generate' | 'chat'; useContext: boolean } = {
+		apiType: 'generate',
+		useContext: false
+	};
 
-  public previousMessages: DBMessage[] = [];
+	public previousMessages: DBMessage[] = [];
 
-  private constructor(id?: number) {
-    if (id) {
-      this.#loadChatSessionFromDB(id);
-    }
-  }
+	private constructor(id?: number) {
+		if (id) {
+			this.#loadChatSessionFromDB(id);
+		}
+	}
 
-  public async setPreviousMessages(): Promise<Partial<DBMessage>[]> {
-    let chatList = await idbQuery.getMessages(this.sessionId);
+	public async getPreviousMessages(): Promise<Partial<DBMessage>[]> {
+		let chatList = await idbQuery.getMessages(this.sessionId);
 
-    this.previousMessages = chatList.map((e) => ({
-      content: e.content,
-      role: e.role,
-      images: e?.images?.base64,
-    }));
+		this.previousMessages = chatList.map((e) => e);
 
-    return this.previousMessages;
-  }
-  /**
-   * Creates a user message in the chat session.
-   *
-   * @param content - The content of the message.
-   * @param images - Optional images to be attached to the message.
-   * @returns A Promise that resolves to the created user message.
-   */
-  async createUserDbMessage({
-    content,
-    images,
-    model,
-  }: {
-    model?: string;
-    content: string;
-    images?: MessageImageType;
-  }) {
-    const urlRegex = /https?:\/\/[^\s]+/g;
-    const urs = content.match(urlRegex);
-    let urls: DBMessage["urls"] = urs ? urs : [];
+		return this.previousMessages;
+	}
+	/**
+	 * Creates a user message in the chat session.
+	 *
+	 * @param content - The content of the message.
+	 * @param images - Optional images to be attached to the message.
+	 * @returns A Promise that resolves to the created user message.
+	 */
+	async createUserDbMessage({
+		content,
+		images,
+		model
+	}: {
+		model?: string;
+		content: string;
+		images?: MessageImageType;
+	}) {
+		const urlRegex = /https?:\/\/[^\s]+/g;
+		const urs = content.match(urlRegex);
+		let urls: DBMessage['urls'] = urs ? urs : [];
 
-    const messageData = {
-      chatId: this.sessionId,
-      content,
-      images,
-      role: "user",
-      status: "done",
-      urls,
-      //
-      model: model ?? this.#SessionDB.dbChat.models?.[0],
-    };
+		const messageData = {
+			chatId: this.sessionId,
+			content,
+			images,
+			role: 'user',
+			status: 'done',
+			urls,
+			//
+			model: model ?? this.#SessionDB.dbChat.models?.[0]
+		};
 
-    this.#SessionDB.userDbMessage = await idbQuery.insertMessage(
-      this.#sessionId,
-      messageData
-    );
+		this.#SessionDB.userDbMessage = await idbQuery.insertMessage(this.sessionId, messageData);
 
-    return this.#SessionDB.userDbMessage;
-  }
+		return this.#SessionDB.userDbMessage;
+	}
 
-  async createAssistantMessage(model: string) {
-    const messageData = {
-      chatId: this.#sessionId,
-      model,
-      role: "assistant",
-      status: "idle",
-    };
-    const assistantsDbMessages = await idbQuery.insertMessage(
-      this.#sessionId,
-      messageData
-    );
+	async createUserChatMessage({
+		content,
+		images,
+		model
+	}: {
+		model?: string;
+		content: string;
+		images?: MessageImageType;
+	}) {
+		// format message for chat send
+		this.#SessionDB.userChatMessage = {
+			content: content,
+			role: OllamaChatMessageRole.USER,
+			images: images?.base64 ? [images?.base64] : []
+		};
 
-    console.log("created :", assistantsDbMessages);
+		return this.#SessionDB.userChatMessage;
+	}
 
-    return assistantsDbMessages;
-  }
+	async createDbMessage(
+		role: OllamaChatMessageRole | string,
+		message: Message & {
+			chatId?: number;
+			status?: DBMessage['status'];
+			model?: string;
+		}
+	) {
+		const { status, chatId, model } = message;
+		const messageData: Partial<DBMessage> = {
+			...ChatSessionManager.convertMessage('MessageToDbMessage', message),
+			role: role as OllamaChatMessageRole,
+			chatId: chatId ?? this.sessionId,
+			status: status ?? 'idle',
+			model: model ?? this.#SessionDB.dbChat.models?.[0]
+		};
 
-  async createUserChatMessage({
-    content,
-    images,
-    model,
-  }: {
-    model?: string;
-    content: string;
-    images?: MessageImageType;
-  }) {
-    // format message for chat send
-    this.#SessionDB.userChatMessage = {
-      content: content,
-      role: OllamaChatMessageRole.USER,
-      images: images?.base64 ? [images?.base64] : undefined,
-    };
+		return await idbQuery.insertMessage(this.sessionId, messageData);
+	}
 
-    return this.#SessionDB.userChatMessage;
-  }
-  #loadChatSessionFromDB = (id: number) => {
-    idbQuery
-      .getChat(id)
-      .then((dbChat: DbChat | undefined) => {
-        this.#SessionDB.dbChat = dbChat ?? ({} as Partial<DbChat>);
-        this.#sessionId = dbChat?.id;
-        console.log("chat loaded", this.#SessionDB.dbChat);
-      })
-      .catch((error) => {
-        console.error("Error loading chat:", error);
-      });
-  };
+	async createSystemMessage(
+		message: Message & {
+			chatId?: number;
+			status: DBMessage['status'];
+			model: string;
+		}
+	) {
+		const existingSystemMessage = await idbQuery.getSystemMessage(this.sessionId);
+		if (existingSystemMessage) {
+			return existingSystemMessage;
+		}
+		return this.createDbMessage(OllamaChatMessageRole.SYSTEM, message);
+	}
 
-  static loadSession() {
-    return new ChatSessionManager();
-  }
+	#loadChatSessionFromDB = (id: number) => {
+		idbQuery
+			.getChat(id)
+			.then((dbChat: DbChat | undefined) => {
+				this.#SessionDB.dbChat = dbChat ?? ({} as Partial<DbChat>);
+				this.#SessionDB.sessionId = dbChat?.id;
+			})
+			.catch((error) => {
+				console.error('Error loading chat:', error);
+			});
+	};
 
-  get ChatSessionDB() {
-    return {
-      createSession: this.#createChatSessionInDB.bind(this),
-      updateSession: this.#updateChatSessionInDB.bind(this),
-      /*  init: this.#initChatDb.bind(this), */
-    };
-  }
+	static loadSession() {
+		return new ChatSessionManager();
+	}
 
-  async #createChatSessionInDB(chatData: Partial<DbChat> = {} as DbChat) {
-    this.#SessionDB.dbChat = await this.#initChatDb(
-      this.#sessionId,
-      chatData as DbChat
-    );
-    this.#sessionId = this.#SessionDB.dbChat.id;
-    return { sessionId: this.#sessionId, dbChat: this.#SessionDB.dbChat };
-  }
+	get ChatSessionDB() {
+		return {
+			createSession: this.#createChatSessionInDB.bind(this),
+			updateSession: this.#updateChatSessionInDB.bind(this)
+			/*  init: this.#initChatDb.bind(this), */
+		};
+	}
 
-  async #updateChatSessionInDB(chatData: Partial<DbChat> = {} as DbChat) {
-    if (this.#SessionDB.dbChat.id !== undefined) {
-      this.#SessionDB.dbChat = await idbQuery.updateChat(
-        this.#SessionDB.dbChat.id,
-        {
-          ...(chatData as DbChat),
-        } as DbChat
-      );
-    }
-  }
+	async #createChatSessionInDB(chatData: Partial<DbChat> = {} as DbChat) {
+		this.#SessionDB.dbChat = await this.#initChatDb(this.sessionId, chatData as DbChat);
+		this.#SessionDB.sessionId = this.#SessionDB.dbChat.id;
+		return { sessionId: this.sessionId, dbChat: this.#SessionDB.dbChat };
+	}
 
-  /**
-   * Handles the completion of a message in the chat session.
-   * @param assistantMessage - The assistant message object.
-   * @param data - The response data from the assistant.
-   */
-  public async onMessageDone(
-    assistantMessage: DBMessage,
-    data: OllamaResponse
-  ) {
-    await Promise.all([
-      //idbQuery.updateChat(assistantMessage.chatId, { context: data?.context }),
-      idbQuery.updateMessage(assistantMessage.id, { status: "done" }),
-    ]);
-  }
+	async #updateChatSessionInDB(chatData: Partial<DbChat> = {} as DbChat) {
+		if (this.#SessionDB.dbChat.id !== undefined) {
+			this.#SessionDB.dbChat = await idbQuery.updateChat(this.#SessionDB.dbChat.id, {
+				...(chatData as DbChat)
+			} as DbChat);
+			this.#SessionDB.sessionId = this.#SessionDB.dbChat.id;
+			return { sessionId: this.sessionId, dbChat: this.#SessionDB.dbChat };
+		}
+		return undefined;
+	}
 
-  /**
-   * Handles the incoming message stream from the assistant.
-   * @param assistantMessage - The message object received from the assistant.
-   * @param data - The response data received from the assistant.
-   */
-  public async onMessageStream(
-    assistantMessage: DBMessage,
-    data: OllamaResponse
-  ) {
-    await idbQuery.updateMessageStream(assistantMessage.id, data);
-  }
-  async #initChatDb(
-    sessionId?: number,
-    chatData: DbChat = {} as DbChat
-  ): Promise<DbChat> {
-    if (sessionId && Boolean(await idbQuery.getChat(sessionId))) {
-      await idbQuery.updateChat(sessionId, chatData);
-    }
+	/**
+	 * Handles the completion of a message in the chat session.
+	 * @param assistantMessage - The assistant message object.
+	 * @param data - The response data from the assistant.
+	 */
+	public async onMessageDone(assistantMessage: DBMessage, data: OllamaResponse) {
+		await Promise.all([
+			//idbQuery.updateChat(assistantMessage.chatId, { context: data?.context }),
+			idbQuery.updateMessage(assistantMessage.id, { status: 'done' })
+		]);
+	}
 
-    return sessionId && Boolean(await idbQuery.getChat(sessionId))
-      ? ((await idbQuery.getChat(sessionId)) as DbChat)
-      : ((await idbQuery.insertChat(chatData)) as DbChat);
-  }
+	/**
+	 * Handles the incoming message stream from the assistant.
+	 * @param assistantMessage - The message object received from the assistant.
+	 * @param data - The response data received from the assistant.
+	 */
+	public async onMessageStream(assistantMessage: DBMessage, data: OllamaResponse) {
+		await idbQuery.updateMessageStream(assistantMessage.id, data);
+	}
+	async #initChatDb(sessionId?: number, chatData: DbChat = {} as DbChat): Promise<DbChat> {
+		let ret: DbChat;
+		if (sessionId && Boolean(await idbQuery.getChat(sessionId))) {
+			ret = await idbQuery.updateChat(sessionId, chatData);
+		} else {
+			ret = await idbQuery.insertChat(chatData);
+		}
+		return ret;
+	}
 
-  get Db() {
-    return this.#SessionDB;
-  }
-  get sessionId() {
-    return this.#sessionId;
-  }
+	static convertMessage<T extends 'MessageToDbMessage' | 'DbMessageToMessage'>(
+		from: T,
+		message: T extends 'MessageToDbMessage' ? Message : DBMessage
+	): T extends 'MessageToDbMessage' ? DBMessage : Message {
+		if (from === 'MessageToDbMessage') {
+			const msg = message as Message;
+			return {
+				...msg,
+				created_at: new Date(),
+				images: msg.images ? { base64: msg.images[0] } : undefined,
+				role: msg.role as 'system' | 'user' | 'tool' | 'assistant'
+			} as unknown as T extends 'MessageToDbMessage' ? DBMessage : Message;
+		} else {
+			const dbMsg = message as DBMessage;
+			return {
+				role: dbMsg.role,
+				content: dbMsg.content,
+				images: dbMsg.images ? [dbMsg.images.base64] : undefined
+			} as unknown as T extends 'MessageToDbMessage' ? DBMessage : Message;
+		}
+	}
 
-  clearSession() {
-    this.#SessionDB = {
-      dbChat: {},
-      messageAssistant: [],
-      messageUser: {},
-    };
-    this.#sessionId = undefined;
-  }
+	static messageToDbMessage(
+		message: Message,
+		chatId: number,
+		status: DBMessage['status'],
+		model: string
+	): DBMessage {
+		return {
+			id: 0, // This should be set by the database
+			chatId: chatId,
+			content: message.content,
+			created_at: new Date(),
+			images: message.images ? { base64: message.images[0] } : undefined,
+			status: status,
+			context: [],
+			resume: '',
+			model: model,
+			ia_lock: false,
+			role: message.role as 'system' | 'user' | 'tool' | 'assistant'
+		};
+	}
+
+	static dbMessageToMessage(dbMessage: DBMessage): Message {
+		return {
+			role: dbMessage.role,
+			content: dbMessage.content,
+			images: dbMessage.images ? [dbMessage.images.base64] : undefined
+		};
+	}
+
+	get Db() {
+		return this.#SessionDB;
+	}
+	get sessionId() {
+		return this.#SessionDB.sessionId;
+	}
+	get session() {
+		return this.#SessionDB;
+	}
+
+	/**
+	 * Clears the current session data.
+	 */
+	clearSession() {
+		this.#SessionDB = {
+			sessionId: undefined,
+			dbChat: {},
+			messageAssistant: [],
+			messageUser: {}
+		};
+	}
 }
--- src/types/db.ts
+++ src/types/db.ts
@@ -1,258 +1,269 @@
-import type { TplFieldType } from "@medyll/idae-idbql";
-import type { OllamaChat } from "./ollama";
+import type { TplFieldType } from '@medyll/idae-idbql';
+import type { OllamaChat } from './ollama';
+/* 
 
+curl http://localhost:11434/api/generate -d '{
+  "model": "mistral",
+  "prompt": "[INST] why is the sky blue? [/INST]",
+  "raw": true,
+  "stream": false
+}'
+ */
 /**
  * Represents a chat.
  */
 export type DbChat = {
-  id: number;
-  chatId: string;
-  chatPassKey: string;
-  categoryId: number;
-  title: string;
-  description: string;
-  models: string[];
-  created_at: Date;
-  category: string;
-  dateLastMessage: Date;
-  tags: DbTags[];
-  systemPrompt: PromptType;
-  context: number[];
-  ollamaBody: Partial<OllamaChat>;
-  // Ajout suggéré
-  bookId?: number;
+	id: number;
+	chatId: string;
+	chatPassKey: string;
+	categoryId: number;
+	title: string;
+	description: string;
+	models: string[];
+	created_at: Date;
+	category: string;
+	dateLastMessage: Date;
+	tags: DbTags[];
+	systemPrompt: PromptType;
+	context: number[];
+	ollamaBody: Partial<OllamaChat>;
+	// Ajout suggéré
+	bookId?: number;
 };
 export type DbCategory = {
-  id: number;
-  name: string;
-  code: string;
-  ia_lock: boolean;
+	id: number;
+	name: string;
+	code: string;
+	ia_lock: boolean;
 };
 export type DbTags = {
-  id: number;
-  name: string;
-  code: string;
-  ia_lock: boolean;
+	id: number;
+	name: string;
+	code: string;
+	ia_lock: boolean;
 };
 
 /**
  * Represents a chat list type.
  */
 export interface ChatListType {
-  [key: string]: DbChat;
+	[key: string]: DbChat;
 }
 
 /**
  * Represents a message.
  */
 export type DBMessage = {
-  id: number;
-  messageId?: string;
-  chatId: string;
-  content: string;
-  created_at: Date;
-  images?: MessageImageType;
-  status: "idle" | "done" | "sent" | "streaming" | "error";
-  context: number[];
-  resume: string;
-  model: string;
-  ia_lock: boolean;
-  urls?: { url: string; image?: string; order: number; title?: string }[];
+	id: number;
+	messageId?: string;
+	chatId: number;
+	content: string;
+	created_at: Date;
+	images?: MessageImageType;
+	status: 'idle' | 'done' | 'sent' | 'streaming' | 'error';
+	context: number[];
+	resume: string;
+	model?: string;
+	ia_lock?: boolean;
+	urls?: { url: string; image?: string; order: number; title?: string }[];
 } & (
-  | {
-      role: "system";
-    }
-  | {
-      role: "user";
-    }
-  | {
-      role: "assistant";
-      model?: string;
-    }
+	| {
+			role: 'system';
+	  }
+	| {
+			role: 'user';
+	  }
+	| {
+			role: 'tool';
+	  }
+	| {
+			role: 'assistant';
+			model?: string;
+	  }
 );
 
 export type DbPrimitive =
-  | "date"
-  | "text"
-  | "number"
-  | "boolean"
-  | "array"
-  | "object"
-  | `${string}.${string}`;
+	| 'date'
+	| 'text'
+	| 'number'
+	| 'boolean'
+	| 'array'
+	| 'object'
+	| `${string}.${string}`;
 export type DbFieldTypes =
-  | DbPrimitive
-  | `array-of-${DbPrimitive}`
-  | `object-${DbPrimitive}`
-  | `fk-${DbPrimitive}`;
+	| DbPrimitive
+	| `array-of-${DbPrimitive}`
+	| `object-${DbPrimitive}`
+	| `fk-${DbPrimitive}`;
 
 export type DbTemplateModel<TPL> = {
-  [COL in keyof TPL]: {
-    fields: {
-      [T in keyof TPL[COL]]: TPL[COL][T]; //extends { model: infer M } ? DbTemplate<M> : never;
-    };
-  };
+	[COL in keyof TPL]: {
+		fields: {
+			[T in keyof TPL[COL]]: TPL[COL][T]; //extends { model: infer M } ? DbTemplate<M> : never;
+		};
+	};
 };
 
 export type DbTemplate<T> = {
-  fields: {
-    [K in keyof T]: TplFieldType;
-  };
+	fields: {
+		[K in keyof T]: TplFieldType;
+	};
 };
 export type DBAgent = {
-  id: number;
-  name: string;
-  code: string;
-  model: string;
-  prompt: string;
-  agentPromptId: number;
-  ia_lock: boolean;
-  created_at: Date;
-  //
-  specialization?:
-    | "character development"
-    | "plot outline"
-    | "world building"
-    | "dialogue"
-    | "general";
+	id: number;
+	name: string;
+	code: string;
+	model: string;
+	prompt: string;
+	agentPromptId: number;
+	ia_lock: boolean;
+	created_at: Date;
+	//
+	specialization?:
+		| 'character development'
+		| 'plot outline'
+		| 'world building'
+		| 'dialogue'
+		| 'general';
 };
 
 export type DbAgentPrompt = {
-  id: number;
-  created_at: Date;
-  value: string;
-  name: string;
-  code: string;
-  ia_lock: boolean;
+	id: number;
+	created_at: Date;
+	value: string;
+	name: string;
+	code: string;
+	ia_lock: boolean;
 }; //
 
 export type DbAgentOf = {
-  id: number;
-  created_at: Date;
-  value: string;
-  name: string;
-  code: string;
-  collection: string;
-  collectionId: number;
-  ia_lock: boolean;
+	id: number;
+	created_at: Date;
+	value: string;
+	name: string;
+	code: string;
+	collection: string;
+	collectionId: number;
+	ia_lock: boolean;
 }; //
 export type DbMessageListType = {
-  [key: string]: DBMessage;
+	[key: string]: DBMessage;
 };
 
 export type MessageImageType = {
-  name: string;
-  type: string;
-  dataUri: string;
-  header: string;
-  base64: string;
+	name: string;
+	type: string;
+	dataUri: string;
+	header: string;
+	base64: string;
 };
 
 export type PromptType = {
-  id: string;
-  createdAt: Date;
-  value: string;
-  title: string;
-  code: string;
-  ia_lock: boolean;
+	id: string;
+	createdAt: Date;
+	value: string;
+	title: string;
+	code: string;
+	ia_lock: boolean;
 };
 
 // BOOKER
 // Nouvelles interfaces pour le Book Creator Helper
 
 export interface DbBook {
-  id?: number;
-  userId: number;
-  title: string;
-  description: string;
-  created_at: Date;
-  updated_at: Date;
-  status: "draft" | "in_progress" | "completed" | "published";
-  ia_lock: boolean;
+	id?: number;
+	userId: number;
+	title: string;
+	description: string;
+	created_at: Date;
+	updated_at: Date;
+	status: 'draft' | 'in_progress' | 'completed' | 'published';
+	ia_lock: boolean;
 }
 
 export interface DbChapter {
-  id?: number;
-  bookId: number;
-  title: string;
-  content: string;
-  order: number;
-  created_at: Date;
-  updated_at: Date;
-  ia_lock: boolean;
+	id?: number;
+	bookId: number;
+	title: string;
+	content: string;
+	order: number;
+	created_at: Date;
+	updated_at: Date;
+	ia_lock: boolean;
 }
 
 export interface DbWritingGoal {
-  id?: number;
-  userId: number;
-  bookId: number;
-  description: string;
-  targetWordCount: number;
-  deadline: Date;
-  created_at: Date;
-  updated_at: Date;
-  ia_lock: boolean;
+	id?: number;
+	userId: number;
+	bookId: number;
+	description: string;
+	targetWordCount: number;
+	deadline: Date;
+	created_at: Date;
+	updated_at: Date;
+	ia_lock: boolean;
 }
 
 export interface DbCharacter {
-  id?: number;
-  bookId: number;
-  characterLinkId: number[];
-  firstName: string;
-  lastName: string;
-  nickname?: string;
-  role: "protagonist" | "antagonist" | "supporting" | "minor";
-  description: string;
-  backstory?: string;
-  age?: number;
-  gender?: string;
-  occupation?: string;
-  physicalDescription?: string;
-  personalityTraits: string[];
-  goals?: string;
-  conflicts?: string;
-  created_at: Date;
-  updated_at: Date;
-  ia_lock: boolean;
+	id?: number;
+	bookId: number;
+	characterLinkId: number[];
+	firstName: string;
+	lastName: string;
+	nickname?: string;
+	role: 'protagonist' | 'antagonist' | 'supporting' | 'minor';
+	description: string;
+	backstory?: string;
+	age?: number;
+	gender?: string;
+	occupation?: string;
+	physicalDescription?: string;
+	personalityTraits: string[];
+	goals?: string;
+	conflicts?: string;
+	created_at: Date;
+	updated_at: Date;
+	ia_lock: boolean;
 }
 
 export interface DbCharacterChapterStatus {
-  id?: number;
-  characterId: number;
-  chapterId: number;
-  status: "present" | "mentioned" | "absent";
-  role: "major" | "minor" | "background";
-  actions: string;
-  development: string;
-  notes: string;
-  created_at: Date;
-  updated_at: Date;
+	id?: number;
+	characterId: number;
+	chapterId: number;
+	status: 'present' | 'mentioned' | 'absent';
+	role: 'major' | 'minor' | 'background';
+	actions: string;
+	development: string;
+	notes: string;
+	created_at: Date;
+	updated_at: Date;
 }
 
 export interface DbBookPrompts {
-  id?: number;
-  bookId: number;
-  name: string;
-  category: "character" | "plot" | "setting" | "dialogue" | "general";
-  content: string;
-  created_at: Date;
-  updated_at: Date;
-  ia_lock: boolean;
+	id?: number;
+	bookId: number;
+	name: string;
+	category: 'character' | 'plot' | 'setting' | 'dialogue' | 'general';
+	content: string;
+	created_at: Date;
+	updated_at: Date;
+	ia_lock: boolean;
 }
 
 export interface DbCharacterLink {
-  id: string;
-  characterId: number[];
-  type: string;
-  description: string;
-  active: boolean;
+	id: string;
+	characterId: number[];
+	type: string;
+	description: string;
+	active: boolean;
 }
 
 export interface DbSpaces {
-  id: number;
-  code: string;
-  name: string;
-  promptTypeId: number[];
-  active: boolean;
-  created_at: Date;
-  updated_at: Date;
+	id: number;
+	code: string;
+	name: string;
+	promptTypeId: number[];
+	active: boolean;
+	created_at: Date;
+	updated_at: Date;
 }
