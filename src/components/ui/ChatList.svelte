3b1eb73c4e1b9a353d3ccb709cd696530d94653d
medyll <43032494+medyll@users.noreply.github.com>
2025-03-05 22:31:29 +0100

    refactor: update ChatList to use dbQuery for improved data handling and clarity

--- src/components/ui/ChatList.svelte
+++ src/components/ui/ChatList.svelte
@@ -1,30 +1,26 @@
 <script lang="ts">
 	import { MenuList, MenuListItem, ListTitle } from '@medyll/idae-slotui-svelte';
-	import {
-		getTimeTitle,
-		groupChatMessages,
-		groupMessages
-	} from '$lib/tools/chatMenuList.svelte.js';
+	import { getTimeTitle, groupChatMessages, groupMessages } from '$lib/tools/chatMenuList.svelte.js';
 	import ChatMenuItem from '$components/ui/ChatMenuItem.svelte';
 	import { t } from '$lib/stores/i18n';
 	import { ui } from '$lib/stores/ui';
 	import { engine } from '$lib/tools/engine';
 	import { idbqlState } from '$lib/db/dbSchema';
 	import { page } from '$app/stores';
+	import { dbQuery } from '$lib/db/dbQuery';
 
 	const loadChat = async (chatPassKey: string) => {
 		ui.showHideMenu(false);
 		engine.goto(`/chat/${chatPassKey}`);
 	};
 
-	let chatMenuList = $derived(idbqlState.chat.getAll());
+	let chatList = $derived(dbQuery('chat').getAll());
 	let groupedMenuList = $state({});
-	$effect(() => {
-		chatMenuList;
-		groupedMenuList = groupMessages(chatMenuList);
-	});
 
-	$inspect(groupedMenuList, 'groupedMenuList');
+	$effect(() => {
+		chatList;
+		groupedMenuList = groupMessages(chatList);
+	});
 </script>
 
 <MenuList style="width:100%" selectorField="code" data={groupedMenuList ?? []}>
