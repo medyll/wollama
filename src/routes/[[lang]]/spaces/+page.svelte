d2f45d1fc6fd0cec46d77f744a09a6c3d8cecdfc
medyll <43032494+medyll@users.noreply.github.com>
2025-03-09 04:38:14 +0100

    refactor: remove unused imports and simplify page structure in +page.svelte

--- src/routes/[[lang]]/spaces/+page.svelte
+++ src/routes/[[lang]]/spaces/+page.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import { qoolie } from '$lib/db/dbQuery';
 	import CollectionButton from '$components/form/CollectionButton.svelte';
 	import CollectionList from '$components/form/CollectionList.svelte';
 
-	export let data: PageData;
 
-	const spaceList = qoolie('space').getAll();
- 
+
 </script>
 
 <CollectionList collection="space">
