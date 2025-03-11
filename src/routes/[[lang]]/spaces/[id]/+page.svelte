aa329b9b9ff1f981c7ed49e34087d174c5fe7af1
medyll <43032494+medyll@users.noreply.github.com>
2025-03-11 14:16:44 +0100

    refactor: enhance CollectionList functionality with load handler and engine integration

---
+++ src/routes/[[lang]]/spaces/[id]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { qoolie } from '$lib/db/dbQuery';
+	import { page } from '$app/state';
+	import FieldValue from '$components/form/FieldValue.svelte';
+	import DataProvider from '$components/form/DataProvider.svelte';
+	import CollectionList from '$components/form/CollectionList.svelte';
+
+	let _data = $derived(qoolie('space').getOne(page.params.id));
+</script>
+
+<DataProvider collection="space" data={_data}>
+	<FieldValue data={_data} fieldName="name" showLabel={false} />
+	<FieldValue data={_data} fieldName="caption" showLabel={false} />
+</DataProvider>
+<CollectionList collection="chat" where={{ spaceId: page.params.id }} />
