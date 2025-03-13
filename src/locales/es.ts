88b9cf98c105b0619392cb8f930e3362d80a9ed6
medyll <43032494+medyll@users.noreply.github.com>
2025-03-13 23:06:02 +0100

    refactor: standardize formatting in language files for consistency

--- src/locales/es.ts
+++ src/locales/es.ts
@@ -1,57 +1,57 @@
 export default {
 	es: {
-		prompt:   {
+		prompt  : {
 			promptCenter: 'centro del prompt'
 		},
 		settings: {
-			auth:            'autenticación',
-			avatar:          'avatar',
-			avatar_email:    'email de gravatar',
-			lang:            'idioma',
-			server_url:      'URL del servidor Ollama',
-			enter_model:     'introducir un nombre de modelo para descargar',
-			system_prompt:   'prompt del sistema',
-			delete_model:    'eliminar modelo',
+			auth           : 'autenticación',
+			avatar         : 'avatar',
+			avatar_email   : 'email de gravatar',
+			lang           : 'idioma',
+			server_url     : 'URL del servidor Ollama',
+			enter_model    : 'introducir un nombre de modelo para descargar',
+			system_prompt  : 'prompt del sistema',
+			delete_model   : 'eliminar modelo',
 			test_connection: 'probar conexión',
-			default_model:   'introducir modelo por defecto',
-			title_auto:      'generación automática de título',
-			model_delete:    'eliminar modelo',
+			default_model  : 'introducir modelo por defecto',
+			title_auto     : 'generación automática de título',
+			model_delete   : 'eliminar modelo',
 			voice_auto_stop: 'parada automática de voz',
-			modules:         {
-				addons:   'complementos',
+			modules        : {
+				addons  : 'complementos',
 				advanced: 'avanzado',
-				general:  'general',
-				infos:    'información',
-				models:   'modelos'
+				general : 'general',
+				infos   : 'información',
+				models  : 'modelos'
 			},
-			theme:           'tema',
-			pull_model:      'introducir un nombre de modelo para descargar',
-			theme_dark:      'oscuro',
-			request_mode:    'simple',
-			theme_light:     'claro',
-			resetAll:        'restablecer todo'
+			theme          : 'tema',
+			pull_model     : 'introducir un nombre de modelo para descargar',
+			theme_dark     : 'oscuro',
+			request_mode   : 'simple',
+			theme_light    : 'claro',
+			resetAll       : 'restablecer todo'
 		},
-		status:   {
-			connected:  'conectado',
+		status  : {
+			connected : 'conectado',
 			connecting: 'conectando',
-			error:      'error'
+			error     : 'error'
 		},
-		ui:       {
-			aiCautionMessage:      'la llama puede tener algunas alucinaciones',
+		ui      : {
+			aiCautionMessage     : 'la llama puede tener algunas alucinaciones',
 			messageRole_assistant: 'asistente',
-			myChats:               'mis chats',
-			lastWeek:              'la semana pasada',
-			newChat:               'crear nuevo chat',
-			messageRole_user:      'usuario',
-			settings:              'ajustes',
-			noChats:               'sin chats',
-			signOut:               'cerrar sesión',
-			retryInSeconds:        'reintentar en {{seconds}} segundos',
-			userProfile:           'perfil de usuario',
-			searchChats:           'buscar chats',
-			startondate:           'fecha de inicio',
-			thisWeek:              'esta semana',
-			use_model:             'usar modelo'
+			myChats              : 'mis chats',
+			lastWeek             : 'la semana pasada',
+			newChat              : 'crear nuevo chat',
+			messageRole_user     : 'usuario',
+			settings             : 'ajustes',
+			noChats              : 'sin chats',
+			signOut              : 'cerrar sesión',
+			retryInSeconds       : 'reintentar en {{seconds}} segundos',
+			userProfile          : 'perfil de usuario',
+			searchChats          : 'buscar chats',
+			startondate          : 'fecha de inicio',
+			thisWeek             : 'esta semana',
+			use_model            : 'usar modelo'
 		}
 	}
 };
--- src/locales/it.ts
+++ src/locales/it.ts
@@ -1,55 +1,55 @@
 export default {
 	it: {
 		settings: {
-			auth:            'autenticazione',
-			avatar:          'avatar',
-			avatar_email:    'email da gravatar',
-			lang:            'lingua',
-			enter_model:     'inserisci un nome modello da scaricare',
-			server_url:      'URL del server Ollama',
-			default_model:   'inserisci modello predefinito',
-			system_prompt:   'prompt di sistema',
-			model_delete:    'elimina modello',
+			auth           : 'autenticazione',
+			avatar         : 'avatar',
+			avatar_email   : 'email da gravatar',
+			lang           : 'lingua',
+			enter_model    : 'inserisci un nome modello da scaricare',
+			server_url     : 'URL del server Ollama',
+			default_model  : 'inserisci modello predefinito',
+			system_prompt  : 'prompt di sistema',
+			model_delete   : 'elimina modello',
 			test_connection: 'testa connessione',
-			delete_model:    'elimina modello',
-			title_auto:      'generazione automatica del titolo',
-			modules:         {
-				addons:   'componenti aggiuntivi',
+			delete_model   : 'elimina modello',
+			title_auto     : 'generazione automatica del titolo',
+			modules        : {
+				addons  : 'componenti aggiuntivi',
 				advanced: 'avanzato',
-				infos:    'informazioni',
-				general:  'generale',
-				models:   'modelli'
+				infos   : 'informazioni',
+				general : 'generale',
+				models  : 'modelli'
 			},
 			voice_auto_stop: 'arresto automatico della voce',
-			pull_model:      'inserisci un nome modello da scaricare',
-			theme:           'tema',
-			request_mode:    'semplice',
-			theme_dark:      'scuro',
-			resetAll:        'resetta tutto',
-			theme_light:     'chiaro'
+			pull_model     : 'inserisci un nome modello da scaricare',
+			theme          : 'tema',
+			request_mode   : 'semplice',
+			theme_dark     : 'scuro',
+			resetAll       : 'resetta tutto',
+			theme_light    : 'chiaro'
 		},
-		status:   {
-			connected:  'connesso',
+		status  : {
+			connected : 'connesso',
 			connecting: 'collegamento',
-			error:      'errore'
+			error     : 'errore'
 		},
-		ui:       {
-			aiCautionMessage:      'il lama può avere alcune allucinazioni',
+		ui      : {
+			aiCautionMessage     : 'il lama può avere alcune allucinazioni',
 			messageRole_assistant: 'assistente',
-			lastWeek:              'settimana scorsa',
-			myChats:               'le mie chat',
-			messageRole_user:      'utente',
-			newChat:               'crea nuova chat',
-			noChats:               'nessuna chat',
-			settings:              'impostazioni',
-			promptCenter:          'centro del prompt',
-			signOut:               'disconnettersi',
-			retryInSeconds:        'riprova in {{seconds}} secondi',
-			userProfile:           'profilo utente',
-			searchChats:           'cerca chat',
-			startondate:           'data di inizio',
-			thisWeek:              'questa settimana',
-			use_model:             'usa modello'
+			lastWeek             : 'settimana scorsa',
+			myChats              : 'le mie chat',
+			messageRole_user     : 'utente',
+			newChat              : 'crea nuova chat',
+			noChats              : 'nessuna chat',
+			settings             : 'impostazioni',
+			promptCenter         : 'centro del prompt',
+			signOut              : 'disconnettersi',
+			retryInSeconds       : 'riprova in {{seconds}} secondi',
+			userProfile          : 'profilo utente',
+			searchChats          : 'cerca chat',
+			startondate          : 'data di inizio',
+			thisWeek             : 'questa settimana',
+			use_model            : 'usa modello'
 		}
 	}
 };
